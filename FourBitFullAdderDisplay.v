4442e8c93003f4ba6693415e35754386dd14c3d0
fjt227-debug <fjt227@lehigh.edu>
2025-09-17 18:25:53 -0400

    Update FourBitFullAdderDisplay.v

--- FourBitFullAdderDisplay.v
+++ FourBitFullAdderDisplay.v
@@ -1,1 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/17/2025 05:03:47 PM
+// Design Name: 
+// Module Name: FourBitFullAdderDisplay
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FourBitFullAdderDisplay(
+    
+endmodule
 
