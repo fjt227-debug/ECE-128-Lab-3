05b77bf53cfb3f63f6c8fc679cf3b9683a0f8e47
fjt227-debug <fjt227@lehigh.edu>
2025-09-18 17:29:48 -0400

    Update TestBenchLab3.v

--- TestBenchLab3.v
+++ TestBenchLab3.v
@@ -1,26 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/18/2025 01:30:53 PM
+// Design Name: 
+// Module Name: TestBenchLab3
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
-module TB_Lab3();
-  reg [3:0] A,B;
+
+module TestBenchLab3();
+  reg [3:0] A1,B1;
   reg CI;
   wire [6:0] seg;
   wire [3:0] an;
   wire dp;
   wire CO;
 
-  FourBitFullAdderDisplay uut( .A(A), .B(B), .CI(CI), .seg(seg), .an(an), .dp(dp), .CO(CO));
+  FourBitFullAdderDisplay uut( .A1(A1), .B1(B1), .CI(CI), .seg(seg), .an(an), .dp(dp), .CO(CO));
 
   integer i;
   integer j;
 
   initial begin 
-    A = 4'b0000;
-    B = 4'b0000;
+    A1 = 4'b0000;
+    B1 = 4'b0000;
     CI = 1'b0;
 
     for(i = 0; i < 16; i = i + 1) begin
-      A = i;
+      A1 = i;
       for( j = 0; j < 16; j = j + 1) begin
-        B = j;
+        B1 = j;
 
         CI = 1'b0;
         #10;
@@ -31,4 +52,3 @@ module TB_Lab3();
     $stop;
   end
 endmodule
-        
